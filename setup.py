eb1a922a52cfa80de9812ba4fb4edb19e1564c69
mateoAlea <msalmeron@alea-soluciones.com>
2021-03-29 11:20:03 +0200

    [FEATURE] Add Mysql client Factory class

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setup(name="mysql_wrapper",
       platforms="Linux",
       packages=find_packages(
           exclude=["tests", "specs", "integration_specs", "functional_specs", "acceptance_specs"]),
-      install_requires=['mysqlclient==1.4.1',
-                        'retrying==1.3.3',],
+      install_requires=[
+          'mysqlclient==1.4.1',
+          'retrying==1.3.3',
+      ],
       extras_require={
           'dev': [
               'packaging@https://github.com/aleasoluciones/pydevlib.git#egg=pydevlib',
-          ]
+          ],
       },
       dependency_links=[])
