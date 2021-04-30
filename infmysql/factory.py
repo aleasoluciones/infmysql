1e933a07332991725d74fe9e134f6cd67402f573
luisδμ <luisdedios@alea-soluciones.com>
2021-04-30 13:08:04 +0200

    [INTERNAL] Rename env variables

--- infmysql/factory.py
+++ infmysql/factory.py
@@ -7,8 +7,6 @@ from infmysql.client import MySQLClient
 
 def mysql_client(db_uri=None):
     if db_uri is None:
-        db_uri = os.getenv("LOCAL_DB_URI")
+        db_uri = os.getenv("MYSQL_DB_URI")
 
-    return Factory.instance('mysql_client_{}'.format(db_uri),
-                            lambda: MySQLClient(db_uri)
-                            )
+    return Factory.instance(f'mysql_client_{db_uri}', lambda: MySQLClient(db_uri))
