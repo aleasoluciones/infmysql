f85da23d991d3d203d4027eaddbebfaead7d040d
luisddm <luisdedios@alea-soluciones.com>
2021-10-19 10:57:24 +0200

    [INTERNAL] Improve setup and requirements

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from setuptools import setup, find_packages
 setup(name="infmysql",
       version="0.0.1",
       author="Bifer Team",
-      description="Simple mysql client",
+      description="Simple MySQL client",
       platforms="Linux",
-      packages=find_packages(
-          exclude=["tests", "specs", "integration_specs", "functional_specs", "acceptance_specs"]),
+      packages=find_packages(exclude=["specs", "integration_specs"]),
       install_requires=[
           'mysqlclient==1.4.1',
           'retrying==1.3.3',
+          'infcommon'
       ],
-      extras_require={
-          'dev': [
-              'packaging@https://github.com/aleasoluciones/pydevlib.git#egg=pydevlib',
-          ],
-      },
-      dependency_links=[])
+      dependency_links=[
+          'git+https://github.com/aleasoluciones/infcommon3.git#egg=infcommon'
+      ]
+)
